2a65cdf8eef89c8775702cdd586ac4668747e89f
Mariam Araujo <mariam21betancourt@gmail.com>
2023-06-07 13:53:43 -0700

    adds main route and renders according to the url path

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react';
 import './App.css';
 import AuthPage from '../AuthPage/AuthPage';
 import MainPage from '../MainPage/MainPage';
+import { Route, Routes } from 'react-router-dom';
 
 export default function App() {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState({})
   return (
     <main className="App">
       {
         user ? 
-        <MainPage/>
+        <Routes>
+           <Route path='/' element={<MainPage/>}/>
+        </Routes>
         :
         <AuthPage/>
       }
