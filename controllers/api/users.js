9aca58b6b288af1860f95c3264bffbdf43fbc870
Mariam Araujo <mariam21betancourt@gmail.com>
2023-06-07 18:56:19 -0700

    wip: jwt

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,13 +1,32 @@
+const User = require('../../models/user')
+const jwt = require ('jsonwebtoken')
 module.exports = {
     create
   };
   
-  function create(req, res) {
-    res.json({
-      user: {
-        name: req.body.name,
-        email: req.body.email
-      }
-    });
+  // controllers/api/users.js
+
+async function create(req, res) {
+  try {
+    // Add the user to the database
+    const user = await User.create(req.body);
+    const token = createJWT(user);
+    res.json(token);
+
+  } catch (err) {
+    // Client will check for non-2xx status code
+    // 400 = Bad Request
+    res.status(400).json(err);
   }
-  
+}
+
+/*-- Helper Functions --*/
+
+function createJWT(user) {
+  return jwt.sign(
+    // data payload
+    { user },
+    process.env.SECRET,
+    { expiresIn: '24h' }
+  );
+}
