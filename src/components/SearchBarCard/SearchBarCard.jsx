33e0f4717b69686d8458a3540841d26576d316a4
Mariam Araujo <mariam21betancourt@gmail.com>
2023-06-09 14:14:41 -0700

    wip: adds toggle functionality between dictionary and thesaurus search

--- src/components/SearchBarCard/SearchBarCard.jsx
+++ src/components/SearchBarCard/SearchBarCard.jsx
@@ -1,9 +1,12 @@
+import { set } from 'mongoose';
 import React, { useState } from 'react'
 
 
 export default function SearchBarCard() {
     const [searchQuery, setSearchQuery] = useState('');
     const [searchResults, setSearchResults] = useState([]);
+    const [dictionarySearch, setDictionarySearch] = useState(true)
+    const [thesaurusSearch, setThesaurusSearch] = useState(false)
 
     const handleSearch = async() => {
         try {
@@ -11,12 +14,21 @@ export default function SearchBarCard() {
                 `http://127.0.0.1:3001/api/search/${searchQuery}`,
             );
             const data = await response.json();
-            console.log(data)
             setSearchResults(data)
         } catch (error) {
             console.log(error);
         }
     };
+
+    const handleChange = (e) => {
+        if(e.target.name === 'dictionary') {
+            setDictionarySearch(true);
+            setThesaurusSearch(false)
+        } else {
+            setDictionarySearch(false);
+            setThesaurusSearch(true)
+        }
+    }
     console.log(searchResults)
     return (
         <div>
@@ -25,6 +37,19 @@ export default function SearchBarCard() {
                 value={searchQuery}
                 onChange={(e) => setSearchQuery(e.target.value)}
             />
+            <label> Dictionary
+                <input 
+                type='radio'
+                name='dictionary'
+                checked={dictionarySearch} onChange={handleChange}
+                />
+            </label>
+            <label> Thesaurus
+                <input
+                type="radio"
+                name="thesaurus" 
+                checked={thesaurusSearch} onChange={handleChange} />
+            </label>
             <button onClick={handleSearch}>Search</button>
             {/* <ul>
                 {searchResults.map(result => (
