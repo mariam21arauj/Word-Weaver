a873c47e658b592aa7595eb8b261e0f0d1a4a3a9
Mariam Araujo <mariam21betancourt@gmail.com>
2023-06-14 21:13:55 -0700

    adds css to welcome page

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,14 +1,21 @@
 // AuthPage.jsx
-
+import './AuthPage.css'
 import { Link } from "react-router-dom";
 
-export default function AuthPage({setUser}) {
+export default function AuthPage() {
   return (
     <main>
-      <h2>Welcome</h2>
-      <Link to='/signup'>Sign Up</Link>
-      &nbsp; | &nbsp;
-      <Link to='/login'>Login</Link>
+      <div className="d-flex flex-column justify-content-center w-100 h-100">
+	      <div id='container' className="d-flex flex-column justify-content-center align-items-center">
+		      <h1 className="fw-light text-white m-0">Welcome To Word Weaver</h1>
+		      <div className="btn-group my-5">
+            <Link className="btn btn-outline-light" to='/login'>Login</Link>
+            &nbsp; | &nbsp;
+            <Link className="btn btn-outline-light" to='/signup'>Sign Up</Link>
+		      </div>
+		      <h5 className="fw-light text-white m-0">— By Mariam Araujo —</h5>
+	      </div>
+      </div>
     </main>
   );
 }
