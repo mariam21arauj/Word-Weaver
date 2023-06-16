45a9dc6caddc5e9225a4b1327fb738999cdfba97
Mariam Araujo <mariam21betancourt@gmail.com>
2023-06-16 01:08:45 -0700

    wip: main page css

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -7,9 +7,16 @@ export default function MainPage() {
   }
 
   return (
-    <div>
-      <h1>Main Page</h1>
-      <img className="mainpagepic" src="https://i.postimg.cc/Qxy7bZ5t/Mainpage-Word-Weaver.png" alt="" />
+    <div id="main-container">
+      <div id="mainMessage-container">
+        <h1>Welcome to Word Weaver</h1>
+        <h2>The app that helps you create meaningful, beautiful, and powerful writing</h2>
+        <p>Check the Dictionary section to find words and learn about their definitions, examples, and parts of speech. Or toggle right there to find out about synonyms as well!</p>
+        <p>This app also features a Favorite Words section where you can add the new words your learn and love, and use them as word journal to refer to in your next writing project! </p>
+      </div>
+      <div>
+        <img className="mainpagepic" src="https://i.postimg.cc/Qxy7bZ5t/Mainpage-Word-Weaver.png" alt="" />
+      </div>
     </div>
   );
 }
